03b6f678ef13201ed4176c95e949b75a855c1493
Fernando Marson <fernando.marson@gmail.com>
2024-09-11 18:22:15 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+2. Classe Filme
+
+Atributos:
+
+- String titulo
+- String diretor
+- String resumo
+- int duracao (em minutos)
+- int anoLancamento
+- double avaliacao (até 5)
+
+Construtores:
+
+- Construtor padrão
+- Construtor com todos os atributos
+- Construtor com titulo, diretor e anoLancamento
+
+Métodos:
+
+- Getters e Setters para todos os atributos
+- public void atualizarAvaliacao(double novaAvaliacao)
+- public String getResumo()
+*/
+
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+
+    
+  }
+}
